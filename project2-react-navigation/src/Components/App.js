eec592c2af8e1be519599ebcdeb35601a43436c8
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-13 23:38:54 -0400

    update main app to only include the Nav compoenent. styling was being messed up by view present in App.js

--- project2-react-navigation/src/Components/App.js
+++ project2-react-navigation/src/Components/App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,18 +5,6 @@ import Nav from './Nav';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <Nav/>
-      <StatusBar style="light" />
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
