c806e9badb8e2c8090b9c0ca636883f7d76fb4cc
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-13 23:02:53 -0400

    simple about page component

---
+++ project2-react-navigation/src/Components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default class About extends React.Component {
+    constructor() {
+        super(),
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>About</Text>
+            </View>
+        )
+    }
+} 
